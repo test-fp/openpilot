b13f8fe801a6579b9cc2bceb887aaed52d0a1922
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-25 09:20:26 +0300

    Update visual_settings.h

--- selfdrive/frogpilot/ui/qt/offroad/visual_settings.h
+++ selfdrive/frogpilot/ui/qt/offroad/visual_settings.h
@@ -38,7 +38,7 @@ private:
   std::set<QString> developerWidgetKeys = {"AdjacentLeadsUI", "RadarTracksUI", "ShowStoppingPoint"};
   std::set<QString> modelUIKeys = {"DynamicPathWidth", "LaneLinesWidth", "PathEdgeWidth", "PathWidth", "RoadEdgesWidth", "UnlimitedLength"};
   std::set<QString> navigationUIKeys = {"BigMap", "MapStyle", "RoadNameUI", "ShowSpeedLimits", "UseVienna"};
-  std::set<QString> qualityOfLifeKeys = {"CameraView", "DriverCamera", "StandbyMode", "StoppedTimer"};
+  std::set<QString> qualityOfLifeKeys = {"CameraView", "DriverCamera", "StandbyMode", "HeadlessMode", "StoppedTimer"};
 
   std::set<QString> parentKeys;
 
