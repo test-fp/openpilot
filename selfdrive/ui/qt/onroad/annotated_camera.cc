e63e635a9e1e229bc15e18c0ab98008ba54baba8
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-25 21:05:32 +0300

    Update annotated_camera.cc

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -1141,7 +1141,7 @@ void AnnotatedCameraWidget::updateSignals() {
 void AnnotatedCameraWidget::initializeFrogPilotWidgets() {
   distance_btn = new DistanceButton(this);
   if (this->headLessMode) {
-    main_layout->addWidget(distance_btn, 0, Qt::AlignVCenter | Qt::AlignLeft);
+    main_layout->addWidget(distance_btn, 0, Qt::AlignBottom | Qt::AlignLeft);
   } else {
     main_layout->addWidget(distance_btn, 0, Qt::AlignBottom | Qt::AlignLeft);
   }
