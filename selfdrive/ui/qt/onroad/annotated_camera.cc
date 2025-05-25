e13f18f2d011b3c8ba0a19deced2c4a5d274914b
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-25 16:04:39 +0300

    Update annotated_camera.cc

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -553,12 +553,10 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
       drawText(p, rect().center().x(), 290, QString("%1 seconds").arg(seconds));
     } else {
       p.setFont(InterFont(176, QFont::Bold));
-      if ( 1=1 ) {
-        //qreal posX = max_speed_headless_rect.x() + max_speed_headless_rect.width() + 25;
-        //drawText(p, rect().center().x(), 210, speedStr);
-        drawText(p,(max_speed_headless_rect.x() + (max_speed_headless_rect.width() * 2) + 25) , 210, speedStr);
+      if (this->headLessMode) {
+        drawText(p,(max_speed_headless_rect.x() + max_speed_headless_rect.width() + speed_limit_headless_rect.width() + 25) , 210, speedStr);
         p.setFont(InterFont(66));
-        drawText(p,(max_speed_headless_rect.x() + (max_speed_headless_rect.width() * 2) + 25) , 290, speedUnit, 200);
+        drawText(p,(max_speed_headless_rect.x() + max_speed_headless_rect.width() + speed_limit_headless_rect.width() + 25) , 290, speedUnit, 200);
       } else {
         drawText(p, rect().center().x(), 210, speedStr);
         p.setFont(InterFont(66));
