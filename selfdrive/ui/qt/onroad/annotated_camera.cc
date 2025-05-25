77c400e2e22a17ce04fbdfa5991fd70f4c2085aa
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-25 19:51:07 +0300

    Update annotated_camera.cc

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -164,7 +164,7 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     // else if (has_us_speed_limit && speedLimitStr.size() >=3 ) max_speed_headless_size.setWidth(223); // Might not be relevant if speedLimit is separate
 
     // Shift MAX speed to the right by 120  (it was 60 before)
-    max_speed_headless_rect.setRect(rect().width() * 0.65, common_y_headless, max_speed_headless_size.width(), max_speed_headless_size.height());
+    max_speed_headless_rect.setRect(rect().width() * 0.6, common_y_headless, max_speed_headless_size.width(), max_speed_headless_size.height());
 
     if (!hideMaxSpeed) { //
       // Draw background for MAX speed
