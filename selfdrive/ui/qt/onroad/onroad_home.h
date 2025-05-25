c12a368eae3444bb39927c6d730899be76e0aebf
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-25 10:43:58 +0300

    Update onroad_home.h

--- selfdrive/ui/qt/onroad/onroad_home.h
+++ selfdrive/ui/qt/onroad/onroad_home.h
@@ -23,6 +23,8 @@ private:
   QColor bg = bg_colors[STATUS_DISENGAGED];
   QWidget *map = nullptr;
   QHBoxLayout* split;
+  QVBoxLayout *main_layout; // To store the pointer to your main layout
+  bool prev_headless_mode_state = false; // To track the previous state of headless_mode
 
   // FrogPilot variables
   bool blindSpotLeft;
