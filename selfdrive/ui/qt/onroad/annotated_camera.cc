1ea85625802fa687a55e74a403bc99b90960fa27
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-26 19:54:00 +0300

    Update annotated_camera.cc

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -39,8 +39,8 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   //if (this->headLessMode) {
   // == START MODIFICATION FOR APPROACH 2 ==
   // Create and add distance_btn here, BEFORE other widgets are added to main_layout
-  distance_btn = new DistanceButton(this);
-  buttons_layout->addWidget(distance_btn);
+  //distance_btn = new DistanceButton(this);
+  //buttons_layout->addWidget(distance_btn);
   // == END MODIFICATION FOR APPROACH 2 ==
   //}
 
@@ -1158,9 +1158,10 @@ void AnnotatedCameraWidget::updateSignals() {
 }
 
 void AnnotatedCameraWidget::initializeFrogPilotWidgets() {
-  //distance_btn = new DistanceButton(this);
+  distance_btn = new DistanceButton(this);
   //QSpacerItem *verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
   //main_layout->addWidget(distance_btn, 0, Qt::AlignBottom | Qt::AlignLeft);
+  main_layout->addWidget(distance_btn, 0, Qt::AlignTop | Qt::AlignLeft);
   //main_layout->addItem(verticalSpacer);
 
   //if (this->headLessMode) {
